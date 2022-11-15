4419a4ef511486677a531e97df341750c21c9b1b
DDRBernal <otalorabernal@gmail.com>
2022-11-14 23:50:33 -0500

    menuLists.html updated

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,24 +1,24 @@
-var app = (function () {
+var app = (function() {
     let idRestaurant;
 
-    function login(username, passwd){
-        apiclient.getUserByName(username, passwd, (req,res) => {
+    function login(username, passwd) {
+        apiclient.getUserByName(username, passwd, (req, res) => {
             //login succefully
-            if (res==true){
+            if (res == true) {
                 console.log("Valid");
                 goToSite("restaurant.html");
-            }else{
+            } else {
                 alert("The user doesn't exists or the email/password are invalids");
             }
         });
     }
 
-    function signUp(name,age,phonenumber, email, password,repassword){
-        if (name === "" || email === "" || password==="") alert("The name, email and password can't be empty");
+    function signUp(name, age, phonenumber, email, password, repassword) {
+        if (name === "" || email === "" || password === "") alert("The name, email and password can't be empty");
         else {
-            if (validateDataSignUp(name, email, password,repassword)){
-                apiclient.addNewUser(name,age,phonenumber,email,password,false,(req,res) => {
-                    if (res){
+            if (validateDataSignUp(name, email, password, repassword)) {
+                apiclient.addNewUser(name, age, phonenumber, email, password, false, (req, res) => {
+                    if (res) {
                         goToSite("index.html");
                     }
                 });
@@ -26,56 +26,56 @@ var app = (function () {
         }
     }
 
-    function validateDataSignUp(name,email,password,repassword){
+    function validateDataSignUp(name, email, password, repassword) {
         let isValid = false;
         let validation_email = email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        if (validation_email==null)alert("Insert an valid email");
+        if (validation_email == null) alert("Insert an valid email");
         else {
-            if(password.localeCompare(repassword)){
+            if (password.localeCompare(repassword)) {
                 alert("The passwords are not the same");
-            }else{isValid=true;}
+            } else { isValid = true; }
         }
         return isValid;
     }
 
-    function getTablesByRestaurant(idRestaurant){
-        apiclient.getTablesByRestaurant(idRestaurant, (req,res)=>{
+    function getTablesByRestaurant(idRestaurant) {
+        apiclient.getTablesByRestaurant(idRestaurant, (req, res) => {
 
         });
     }
 
-    function getMenusByRestaurant(){
+    function getMenusByRestaurant() {
         var link = window.location.href;
-        idRestaurant = link.replace("http://localhost:8080/menuLists.html?","");
+        idRestaurant = link.replace("http://localhost:8080/menuLists.html?", "");
         console.log(idRestaurant);
-        apiclient.getMenusByRestaurant(idRestaurant,(req,res)=>{
+        apiclient.getMenusByRestaurant(idRestaurant, (req, res) => {
             createTableMenus(res);
         });
     }
 
-    function getRestaurants(){
-        apiclient.getRestaurants((req,res)=>{
+    function getRestaurants() {
+        apiclient.getRestaurants((req, res) => {
             createTable(res);
         });
     }
 
-    function createTableMenus(data){
+    function createTableMenus(data) {
         //row-starters gy-5
+        console.log(data);
         let table = $("#fl-table1 tbody");
         table.empty();
         var elemento = document.getElementById("starters");
-
         if (data !== undefined) {
-          const datanew = data.map((menu) => {
-              return {
-                  name: menu.name,
-                  price: menu.price,
-                  calories: menu.calories
-              }
-          });
-          datanew.forEach(({name, price,calories}) => {
+            const datanew = data.map((menu) => {
+                return {
+                    name: menu.name,
+                    price: menu.price,
+                    calories: menu.calories
+                }
+            });
+            datanew.forEach(({ name, price, calories }) => {
 
-          elemento.innerHTML +=
+                elemento.innerHTML +=
                     `<div class="col-lg-4 menu-item">
                      <a href="img/menu/menu-item-1.png" class="glightbox"><img src="img/menu/menu-item-1.png" class="menu-img img-fluid" alt=""></a>
                      <h4>${name}</h4>
@@ -85,66 +85,68 @@ var app = (function () {
                      <p class="price">
                        ${price}
                      </p>
+                     <button type="button" class="btn btn-outline-danger">Seleccionar</button>
                    </div><!-- Menu Item -->`
 
-           })
-       }console.log(elemento.innerHTML);
-       }
+            })
+        }
+        console.log(elemento.innerHTML);
+    }
 
-    function createTable(data){
-            let table = $("#fl-table tbody");
-            table.empty();
-            if (data !== undefined) {
-              const datanew = data.map((restaurant) => {
-                  return {
-                      id: restaurant.id,
-                      name: restaurant.name,
-                      address: restaurant.address,
-                      phonenumber: restaurant.phonenumber
-                  }
-              });
-              console.log(datanew);
-              datanew.forEach(({id,name, address, phonenumber}) => {
-              table.append(
-                      `<tr>
+    function createTable(data) {
+        let table = $("#fl-table tbody");
+        table.empty();
+        if (data !== undefined) {
+            const datanew = data.map((restaurant) => {
+                return {
+                    id: restaurant.id,
+                    name: restaurant.name,
+                    address: restaurant.address,
+                    phonenumber: restaurant.phonenumber
+                }
+            });
+            console.log(datanew);
+            datanew.forEach(({ id, name, address, phonenumber }) => {
+                table.append(
+                    `<tr>
                         <td>${name}</td>
                         <td>${address}</td>
                         <td>${phonenumber}</td>
                         <td><button onclick="app.setIdRestaurant('${id}')">Seleccionar</button></td></tr>
                       `
-                  );
-              })
+                );
+            })
         }
     }
 
-    function setIdRestaurant(newid){
-        idRestaurant=newid;
-        console.log("menuLists.html?"+idRestaurant);
-        goToSite("menuLists.html?"+idRestaurant);
+    function setIdRestaurant(newid) {
+        idRestaurant = newid;
+        console.log("menuLists.html?" + idRestaurant);
+        goToSite("menuLists.html?" + idRestaurant);
     }
 
-    function getMenuList(){
-
-    }
-
-    function addNewName(){
+    function getMenuList() {
 
     }
 
-    function goToSite(page){
+    function addNewName() {
+
+    }
+
+    function goToSite(page) {
         window.location.replace(page);
     }
 
 
 
 
-  return {
-    addNewName : addNewName,
-    login : login,
-    signUp : signUp,
-    getMenusByRestaurant : getMenusByRestaurant,
-    getRestaurants : getRestaurants,
-    setIdRestaurant : setIdRestaurant
-  };
+    return {
+        addNewName: addNewName,
+        login: login,
+        signUp: signUp,
+        getMenusByRestaurant: getMenusByRestaurant,
+        getRestaurants: getRestaurants,
+        setIdRestaurant: setIdRestaurant
+    };
 
 })();
