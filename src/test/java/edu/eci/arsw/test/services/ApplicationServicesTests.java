2e3e0f4b9b74d674ef7ccf70dcf2fcf7ab899656
DDRBernal <otalorabernal@gmail.com>
2022-12-05 22:47:30 -0500

    form for add table added

--- src/test/java/edu/eci/arsw/test/services/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/test/services/ApplicationServicesTests.java
@@ -1,4 +1,4 @@
-package edu.eci.arsw.test.services;
+/*package edu.eci.arsw.test.services;
 
 
 import edu.eci.arsw.digital_waiter.database.JavaPostgreSQL;
@@ -157,4 +157,4 @@ public class ApplicationServicesTests {
             Logger.getLogger(ApplicationServicesTests.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-}
+}*/
